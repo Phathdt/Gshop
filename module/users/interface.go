c1efd1b34aba6441c6289fa92f2bbf2d9b99f51c
Phathdt <phathdt379@gmail.com>
2022-01-09 16:56:18 +0700

    move to interface

--- module/users/interface.go
+++ module/users/interface.go
@@ -6,6 +6,12 @@ import (
 	"gshop/module/users/usrmodel"
 )
 
+type UserRepo interface {
+	GetUserByUsername(ctx context.Context, username string) (*usrmodel.User, error)
+	GetUser(ctx context.Context, id uint32) (*usrmodel.User, error)
+	CreateUser(ctx context.Context, input *usrmodel.UserCreate) error
+}
+
 type UserUseCase interface {
 	GetUserByUsername(ctx context.Context, username string) (*usrmodel.User, error)
 	GetUser(ctx context.Context, id uint32) (*usrmodel.User, error)
