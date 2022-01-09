db6f8ede3bd25d8b56014380a5dd327c15943880
Phathdt <phathdt379@gmail.com>
2022-01-09 23:43:40 +0700

    fix issue logger

--- internal/gorm/init.go
+++ internal/gorm/init.go
@@ -4,11 +4,21 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitDb() (*gorm.DB, error) {
+	var logLevel logger.LogLevel
+	if viper.GetString("LOG_LEVEL") == "DEBUG" {
+		logLevel = logger.Info
+	} else {
+		logLevel = logger.Error
+	}
+
 	return gorm.Open(postgres.New(postgres.Config{
 		DSN:                  viper.GetString("DATABASE_URL"),
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		Logger: logger.Default.LogMode(logLevel),
+	})
 }
